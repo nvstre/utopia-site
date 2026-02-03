65134909798e485879e83c73ae52bcd9417ef3e2
cibucristi <cibucristi1@gmail.com>
2026-02-03 21:30:18 +0100

    feat: enhance home component with scroll detection and mobile menu toggle

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule, DOCUMENT, NgClass } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass,
+    CommonModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isScrolled = false;
+  isMobileMenuOpen = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    
+    // Toggle scrolling on the body
+    if (this.isMobileMenuOpen) {
+      this.document.body.style.overflow = 'hidden';
+    } else {
+      this.document.body.style.overflow = 'auto';
+    }
+  }
+
+  // Helper to close menu when a link is clicked
+  closeMenu() {
+    this.isMobileMenuOpen = false;
+    this.document.body.style.overflow = 'auto';
+  }
 
 }
