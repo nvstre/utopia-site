e8013abb96b5bd5d1323aa9de57c58a12996e31b
Cristian C. <81857739+cristiancibu@users.noreply.github.com>
2026-05-29 16:18:33 +0200

    Update discord-redirect.guard.ts

--- src/app/auth/discord-redirect.guard.ts
+++ src/app/auth/discord-redirect.guard.ts
@@ -4,6 +4,6 @@ import { DOCUMENT } from '@angular/common';
 
 export const discordRedirectGuard: CanActivateFn = (route, state) => {
   const document = inject(DOCUMENT);
-  document.location.href = 'https://discord.gg/XpzgtEcBU2'; 
+  document.location.href = 'https://discord.gg/ndW8QTC6fa'; 
   return false;
 };
